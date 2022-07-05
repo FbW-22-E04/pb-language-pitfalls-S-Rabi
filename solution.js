f605cca669a0c5046b62751791dbaca8bb17231f
S Rabi <rabi.sanaye@yahoo.com>
2022-07-05 17:59:44 +0200

    cortected

--- solution.js
+++ solution.js
@@ -1,13 +1,16 @@
 // NaN: NaN "Not-a-Number" is not equal to anything
-let second = "hello";
-console.log(isNaN(second));
-let third = 3;
-console.log(isNaN(third));
-console.log(NaN); // the type is a Number.
+// let second = "hello";
+// console.log(isNaN(second));
+// let third = 3;
+// console.log(isNaN(third));
+// console.log(NaN); // the type is a Number.
 // 2:
-console.log((0.1 * 0.2).toFixed(2));
 // 3:
-let num = 0;
-console.log(num / Infinity);
+
+//
+console.log(isNaN("hello"));
+console.log(isNaN(3));
+console.log((0.1 * 0.2).toFixed(2));
+console.log(Infinity / 0);
 console.log(Infinity / Infinity);
-console.log(num / 1);
+console.log(1 / 0);
