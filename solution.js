fa9d067a915b4c01ddaf2c8307eae20113090131
S Rabi <rabi.sanaye@yahoo.com>
2022-07-05 14:06:23 +0200

    second exercise

--- solution.js
+++ solution.js
@@ -1,1 +1,13 @@
-
+// NaN: NaN "Not-a-Number" is not equal to anything
+let second = "hello";
+console.log("second is NOT a number:", isNaN(second));
+let third = 3;
+console.log("3 is a number:", isNaN(third));
+console.log(NaN); // the type is a Number.
+// 2:
+console.log((0.1 * 0.2).toFixed(2));
+// 3:
+let num = 0;
+console.log(Infinity / num);
+console.log(Infinity / Infinity);
+console.log(1 / num);
